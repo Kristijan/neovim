f624f04039950cc5d14946989ff4194e5a0f2b8a
Kristian Milos <kristian.milos@anz.com>
2023-02-15 16:35:17 +1100

    Added some plugin overrides

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      highlight = {
+        comments_only = false, -- uses treesitter to match keywords in comments only
+      },
+    },
+  },
+}
