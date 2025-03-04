173aee350da9016bafc740408af79ede7d3eef7e
Kristian Milos <kris@kristijan.org>
2025-03-04 16:19:49 +1100

    Function to disable autoformat for specific file types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- Autoformat setting
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+-- You can show the detected FileType of a file using `:set filetype?`
+set_autoformat({ "ruby" }, false)
