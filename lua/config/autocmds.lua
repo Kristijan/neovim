4b5add128cd00ce37a2d611bc77817865800101c
Kristian Milos <kris@kristijan.org>
2025-08-06 12:13:06 +1000

    Disable spell checking for specific file types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -10,3 +10,11 @@ end
 
 -- You can show the detected FileType of a file using `:set filetype?`
 set_autoformat({ "ruby" }, false)
+
+-- Disable spell checking for specific file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
