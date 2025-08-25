28ee7cc11027c5c74ba62461ba2f040bafc94447
Kristian Milos <kris@kristijan.org>
2025-08-25 17:06:33 +1000

    Workaround until https://github.com/LazyVim/LazyVim/pull/6354 is merged

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,25 @@
+-- Fix bufferline until https://github.com/LazyVim/LazyVim/pull/6354 is merged.
+--
+-- return {
+--   {
+--     "akinsho/bufferline.nvim",
+--     opts = {
+--       options = {
+--         always_show_bufferline = true,
+--       },
+--     },
+--   },
+-- }
+
 return {
   {
     "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
     opts = {
       options = {
         always_show_bufferline = true,
